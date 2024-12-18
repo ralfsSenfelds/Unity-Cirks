6d5dbf9f20110d219e55717e21d6fcce3f6a3335
Ralfs <ralfsssenfelds@gmail.com>
2024-12-18 09:05:51 +0200

    Animations and menu buttons

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+
+    public Texture2D[] cursors;
+
+    void Start()
+    {
+        DefaultCursor();
+    }
+
+    public void DefaultCursor()
+    {
+        Cursor.SetCursor(cursors[0], Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    public void OnButton()
+    {
+        Cursor.SetCursor(cursors[1], Vector2.zero, CursorMode.ForceSoftware);
+    }
+    public void ClickedButton()
+    {
+        Cursor.SetCursor(cursors[2], Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    public void OnObject()
+    {
+        Cursor.SetCursor(cursors[3], Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Unity-Cirks
-Unity 2.5D Circus project
+# Cirks_Sinicina
+Unity 2.5D Circus Project
+
 
 ### To do list:
--[ ] Create main menu
--[ ] Add animations to dice and clouds
--[ ] Custom cursor
--[ ] Add settings
--[ ] Add charecter selection screen
--[ ] ...
+- [ ] Create main menu
+- [ ] Add animations to dice and clouds  
+- [X] Change cursor 
+- [ ] Add settings
+- [ ] Add character selection screen
+- [ ] Create Circus game
